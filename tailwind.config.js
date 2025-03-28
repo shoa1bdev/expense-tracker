4eeb02d80c72b9c6535e1c65c0110de66434675f
Shoaib <s.akhtar7958@gmail.com>
2025-03-29 01:00:32 +0530

    Fixed Tailwind issue and updated project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",  // Corrected path for Vite + React
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
